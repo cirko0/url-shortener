e6af42a129d2f30a8a6d643db66b8501cdd1a91a
cirko0 <cirko007ivan@gmail.com>
2023-10-26 20:20:18 +0200

    Added server and DB connection

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ mongoose
   })
   .then(() => {
     console.log("DB connection successful!");
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Listening on port: " + process.env.PORT || 3000);
+    });
   });
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port: " + process.env.PORT || 3000);
-});
